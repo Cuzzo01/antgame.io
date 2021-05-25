fd5e4a35529483c7c358c92001ebd4159fb304e8
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-05-24 19:16:45 -0500

    Set GA environment

--- src/GAInitializer.js
+++ src/GAInitializer.js
@@ -5,5 +5,20 @@ const trackingID = "G-J5Z9WSWYGY";
 export default function GAInitializer() {
   ReactGA.initialize(trackingID);
 
+  let env = "prod";
+
+  const lowestDomain = window.location.host.split(".")[0].toLowerCase();
+  if (lowestDomain === "dev") {
+    console.log("in dev mode");
+    env = "dev";
+  } else if (lowestDomain === "localhost:3000") {
+    console.log("in local mode");
+    env = "local";
+  }
+
+  ReactGA.set({
+    env: env,
+  });
+
   return <div />;
 }
