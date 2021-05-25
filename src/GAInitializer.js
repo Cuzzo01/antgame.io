7681f6847bb6da68f80a5d4ca2b14e77a8fa055c
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-05-24 19:21:36 -0500

    Remove debug messages

--- src/GAInitializer.js
+++ src/GAInitializer.js
@@ -9,10 +9,8 @@ export default function GAInitializer() {
 
   const lowestDomain = window.location.host.split(".")[0].toLowerCase();
   if (lowestDomain === "dev") {
-    console.log("in dev mode");
     env = "dev";
   } else if (lowestDomain === "localhost:3000") {
-    console.log("in local mode");
     env = "local";
   }
 
