07055941e3174d229867583bb965b57b048edc48
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-07-04 19:06:38 -0500

    Fix auth paths

--- AntGameUI/src/antGame/Auth/AuthService.js
+++ AntGameUI/src/antGame/Auth/AuthService.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export async function getToken(username, password) {
   return axios
-    .post("/auth/login", { user: username, pass: password })
+    .post("/api/auth/login", { user: username, pass: password })
     .then((res) => {
       return res.data;
     });
 }
 
 export async function getAnonToken(clientID) {
-  return axios.post("/auth/anonToken", { clientID: clientID }).then((res) => {
+  return axios.post("/api/auth/anonToken", { clientID: clientID }).then((res) => {
     return res.data;
   });
 }
