c897be23ce431395c333195303503e21a44c6aa8
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-07-25 14:16:05 -0500

    Add rank to challenge modal

--- AntGameUI/src/antGame/AntGameHelpers/Challenge/ChallengeModal.jsx
+++ AntGameUI/src/antGame/AntGameHelpers/Challenge/ChallengeModal.jsx
@@ -6,17 +6,26 @@ import AuthHandler from "../../Auth/AuthHandler";
 
 const ChallengeModal = props => {
   const [isWrRun, setIsWrRun] = useState(false);
+  const [records, setRecords] = useState();
 
   useEffect(() => {
     ChallengeHandler.addWrListener(isWrRun => setIsWrRun(isWrRun));
+    ChallengeHandler.addRecordListener(records => setRecords(records));
   });
 
   return (
     <Modal show={props.show} onHide={() => props.closeModal()} backdrop="static" keyboard={false}>
       <Modal.Header className={styles.header}>Results: {props.challengeHandler?.config.name}</Modal.Header>
       <Modal.Body className={styles.body}>
-        {isWrRun ? <h4 className={styles.newWR}>New World Record!</h4> : null}
-        {props.challengeHandler?.isPB ? <h5 className={styles.newPB}>New Personal Record</h5> : null}
+        <div className={styles.runInfo}>
+          {isWrRun ? <h4 className={styles.newWR}>New World Record!</h4> : null}
+          {props.challengeHandler?.isPB ? (
+            <div>
+              <h5 className={styles.newPB}>New Personal Record</h5>
+              <h6>Leaderboard Rank: {records.rank}</h6>
+            </div>
+          ) : null}
+        </div>
         <h5 className={styles.score}>Score</h5>
         <h5>{props.challengeHandler?.score}</h5>
         {AuthHandler.isAnon ? (
