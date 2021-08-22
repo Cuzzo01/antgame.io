6836a9c00bd423201577b91ca58d73772a22d553
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-08-21 19:41:46 -0500

    Allow logins to be disabled

--- AntGameUI/src/antGame/Auth/LoginPage/LoginPage.jsx
+++ AntGameUI/src/antGame/Auth/LoginPage/LoginPage.jsx
@@ -8,18 +8,27 @@ const LoginPage = props => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [formState, setFormState] = useState("");
+  const [allowLogins, setAllowLogins] = useState(true);
+  const [disabledMessage, setDisabledMessage] = useState("");
   const [allowRegistration, setAllowRegistration] = useState(false);
   const history = useHistory();
   const location = useLocation();
 
   useEffect(() => {
-    getFlag("allowAccountRegistration")
-      .then(value => {
-        setAllowRegistration(value);
-      })
-      .catch(e => {
-        console.log(e);
-      });
+    getFlag("allow-logins").then(value => {
+      if (value !== true) {
+        setAllowLogins(false);
+        setDisabledMessage(value);
+      } else {
+        getFlag("allowAccountRegistration")
+          .then(value => {
+            setAllowRegistration(value);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+      }
+    });
   }, []);
 
   function redirectOut() {
@@ -60,52 +69,64 @@ const LoginPage = props => {
 
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>Login</h3>
-      <form onSubmit={handleSubmit}>
-        <div className={styles.inputField}>
-          <label htmlFor="username">Username:</label>
+      <h3 className={`${styles.title} ${styles.bold}`}>Login</h3>
+      {allowLogins ? (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.inputField}>
+              <label htmlFor="username">Username:</label>
+              <br />
+              <input
+                className={styles.input}
+                type="text"
+                name="username"
+                onChange={handleChange}
+                value={username}
+                autoComplete="username"
+              />
+            </div>
+            <div className={styles.inputField}>
+              <label htmlFor="password">Password:</label>
+              <br />
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                onChange={handleChange}
+                value={password}
+                autoComplete="current-password"
+              />
+            </div>
+            {formState === "error" ? (
+              <div className={styles.error}>Login failed, try again</div>
+            ) : null}
+            {formState === "banned" ? <div className={styles.error}>Account banned</div> : null}
+            <input type="submit" style={{ display: "none" }} />
+            <div className={styles.buttonBar}>
+              <div className={`${styles.divButton} ${styles.right}`} onClick={handleSubmit}>
+                Submit
+              </div>
+              <div className={`${styles.divButton} ${styles.left}`} onClick={continueWithoutLogin}>
+                Skip
+                <br />
+                <span className={styles.subtext}>(scores won't save)</span>
+              </div>
+            </div>
+          </form>
           <br />
-          <input
-            className={styles.input}
-            type="text"
-            name="username"
-            onChange={handleChange}
-            value={username}
-            autoComplete="username"
-          />
+          {allowRegistration ? (
+            <div className={styles.registerLink}>
+              <Link to="/register">Create Account</Link>
+            </div>
+          ) : null}
         </div>
-        <div className={styles.inputField}>
-          <label htmlFor="password">Password:</label>
+      ) : (
+        <div className={styles.disabledMessage}>
+          <h5>Logins are currently disabled.</h5>
           <br />
-          <input
-            className={styles.input}
-            type="password"
-            name="password"
-            onChange={handleChange}
-            value={password}
-            autoComplete="current-password"
-          />
+          {disabledMessage}
         </div>
-        {formState === "error" ? <div className={styles.error}>Login failed, try again</div> : null}
-        {formState === "banned" ? <div className={styles.error}>Account banned</div> : null}
-        <input type="submit" style={{ display: "none" }} />
-        <div className={styles.buttonBar}>
-          <div className={`${styles.divButton} ${styles.right}`} onClick={handleSubmit}>
-            Submit
-          </div>
-          <div className={`${styles.divButton} ${styles.left}`} onClick={continueWithoutLogin}>
-            Skip
-            <br />
-            <span className={styles.subtext}>(scores won't save)</span>
-          </div>
-        </div>
-      </form>
-      <br />
-      {allowRegistration ? (
-        <div className={styles.registerLink}>
-          <Link to="/register">Create Account</Link>
-        </div>
-      ) : null}
+      )}
     </div>
   );
 };
