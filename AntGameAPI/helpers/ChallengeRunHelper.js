817ad25a8638315868889de8ebf766081991b840
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-08-21 20:49:44 -0500

    Hotfix

--- AntGameAPI/helpers/ChallengeRunHelper.js
+++ AntGameAPI/helpers/ChallengeRunHelper.js
@@ -6,7 +6,7 @@ const FoodPerCell = 20;
 const DirtPerCell = 50;
 
 const VerifyArtifact = async (runData, clientID) => {
-  if (runData.ClientID !== clientID) return `non-matching clientID : (${clientID, runData.clientID})`;
+  // if (runData.ClientID !== clientID) return `non-matching clientID : (${clientID, runData.clientID})`;
 
   try {
     runData.Score = parseInt(runData.Score);
