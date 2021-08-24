83002ea9cf745778efe4de09b72d9d8d37dcd7ea
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-08-24 17:38:35 -0500

    Disallow whitespace in usernames

--- AntGameAPI/helpers/RegistrationHelper.js
+++ AntGameAPI/helpers/RegistrationHelper.js
@@ -2,9 +2,11 @@ const Filter = require("bad-words");
 const filter = new Filter();
 
 const Blacklist = require("the-big-username-blacklist");
+const usernameRegex = /^\S*$/;
 
 const RegistrationDataSatisfiesCriteria = (username, password, clientID, email) => {
   if (username.length > 20 || username.length < 5) return false;
+  if (!usernameRegex.test(username)) return false;
   if (password.length > 100 || password.length < 8) return false;
   if (!clientID) return false;
   return true;
