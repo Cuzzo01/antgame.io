c55e83bc32a1b67f8c2a39fb2d131b60c621cf64
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-08-24 17:29:26 -0500

    Fix record update bug

--- AntGameAPI/controller/ChallengeController.js
+++ AntGameAPI/controller/ChallengeController.js
@@ -108,9 +108,8 @@ async function postRun(req, res) {
         }
 
         let isWorldRecord = false;
-        let challengeRecord;
+        let challengeRecord = await ChallengeDao.getRecordByChallenge(runData.challengeID);
         if (user.showOnLeaderboard !== false && runData.PB) {
-          challengeRecord = await ChallengeDao.getRecordByChallenge(runData.challengeID);
           const recordEmpty = challengeRecord && Object.keys(challengeRecord).length === 0;
           if (recordEmpty || challengeRecord.score < runData.Score) {
             isWorldRecord = true;
@@ -126,16 +125,18 @@ async function postRun(req, res) {
           }
         }
 
-        if (isWorldRecord)
+        if (isWorldRecord) {
           response.wr = {
             score: runData.Score,
             name: user.username,
           };
-        else if (challengeRecord)
+          response.isWrRun = true;
+        } else if (challengeRecord) {
           response.wr = {
             score: challengeRecord.score,
             name: challengeRecord.username,
           };
+        }
 
         res.send(response);
         return;
