6c270f22e77fbf5bca74ba8b72eba85dc8c1922f
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-08-26 22:01:33 -0500

    Add food to map generator

--- AntGameAPI/MapGenerator/MapGenerator.js
+++ AntGameAPI/MapGenerator/MapGenerator.js
@@ -15,12 +15,16 @@ class Noise {
 
 const BorderSize = 2;
 const WallNoiseFloor = 0.5;
-const WallBaseFreq = 4;
+const FoodNoiseFloor = 0.7;
+const WallBaseFreq = 4.5;
+const FoodFreq = 5;
+const WallFoodBuffer = 2;
 
 // currently (200, 112)
 const generateMap = (width, height) => {
   const noise1 = new Noise(WallBaseFreq);
   const noise2 = new Noise(WallBaseFreq * 2);
+  const noise3 = new Noise(FoodFreq);
 
   let toReturn = [];
   for (let x = 0; x < width; x++) {
@@ -46,7 +50,40 @@ const generateMap = (width, height) => {
     }
   }
 
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      if (toReturn[x][y] === " ") {
+        const nx = x / width - 0.5;
+        const ny = y / height - 0.5;
+
+        const value = noise3.getNoise(nx, ny);
+
+        if (
+          value > FoodNoiseFloor &&
+          AreSurroundingCellsClear(x, y, WallFoodBuffer, ["w"], toReturn)
+        )
+          toReturn[x][y] = "f";
+      }
+    }
+  }
+
   return toReturn;
+};
+
+const AreSurroundingCellsClear = (x, y, radius, clearOf, mapArr) => {
+  for (let i = x - radius; i <= x + radius; i++) {
+    for (let j = y - radius; j <= y + radius; j++) {
+      if (i < 0 || i > mapArr.length - 1) continue;
+      if (j < 0 || j > mapArr[0].length - 1) continue;
+
+      const cellValue = mapArr[i][j];
+      for (let k = 0; k < clearOf.length; k++) {
+        const charToCheck = clearOf[k];
+        if (charToCheck === cellValue) return false;
+      }
+    }
+  }
+  return true;
 };
 
 module.exports = { generateMap };
