55087734ab029c9e8f03f412d56ffd139899a92c
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-08-28 11:10:27 -0500

    Fix run timestamping on userDetails

--- AntGameAPI/controller/AdminController.js
+++ AntGameAPI/controller/AdminController.js
@@ -222,7 +222,7 @@ async function getUserDetails(req, res) {
             score: userDetails.pb,
             runID: userDetails.pbRunID,
             name: challenge.name,
-            runTime: challenge.id.getTimestamp()
+            runTime: userDetails.pbRunID.getTimestamp(),
           };
         }
       });
