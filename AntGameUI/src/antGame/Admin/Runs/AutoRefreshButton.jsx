069b24f6e4d6f0672243e44598234fafc7a5b237
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-09-12 14:37:38 -0500

    Add run auto refresh

---
+++ AntGameUI/src/antGame/Admin/Runs/AutoRefreshButton.jsx
@@ -0,0 +1,57 @@
+import styles from "./RunsList.module.css";
+import adminStyles from "../AdminStyles.module.css";
+import { useEffect, useRef, useState } from "react";
+import Countdown from "react-countdown";
+
+const AutoRefreshButton = props => {
+  const [enabled, setEnabled] = useState(true);
+  const [nextRefreshTime, setNextRefreshTime] = useState(Date.now);
+  const countdownRef = useRef();
+
+  useEffect(() => {
+    if (enabled) {
+      resetRefreshTime();
+      countdownRef.current.getApi().start();
+    }
+  }, [enabled, countdownRef]);
+
+  const handleTimeout = () => {
+    props.onRefresh();
+    resetRefreshTime();
+    countdownRef.current.getApi().start();
+  };
+
+  const resetRefreshTime = () => {
+    const date = new Date();
+    date.setSeconds(date.getSeconds() + 30);
+    setNextRefreshTime(date);
+  };
+
+  return (
+    <div className={styles.refreshButtonContainer}>
+      <span
+        className={`${adminStyles.divButton} ${styles.refreshButton} ${
+          enabled ? styles.enabled : null
+        }`}
+        onClick={() => setEnabled(!enabled)}
+      >
+        {enabled ? (
+          <Countdown
+            ref={countdownRef}
+            autoStart={false}
+            date={nextRefreshTime}
+            onComplete={() => handleTimeout()}
+            renderer={renderer}
+          />
+        ) : (
+          "Live"
+        )}
+      </span>
+    </div>
+  );
+};
+export default AutoRefreshButton;
+
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  return `${seconds - 1}s`;
+};
