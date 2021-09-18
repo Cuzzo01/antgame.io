3eb94d49a892c8211f3c77cc2ac0e32bbaae8fa1
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2021-09-18 12:31:50 -0500

    Fix ants not spawning in sandbox bug

--- AntGameUI/src/antGame/AntGame.js
+++ AntGameUI/src/antGame/AntGame.js
@@ -239,22 +239,22 @@ export default class AntGame extends React.Component {
   updatePlayState = state => {
     const IsChallenge = this.gamemode === "challenge";
     if (state) {
+      debugger;
       if (this.state.emptyMap) return;
       if (this.mapHandler.homeCellCount === 0) return;
       if (IsChallenge && this.timerHandler.noTime) return "reset";
       this.setMapUiUpdate(500);
       this.toggleTimer(true);
-      if (IsChallenge)
-        if (!this.antHandler.antsSpawned) {
-          this.antHandler.spawnAnts(this.homeTrailHandler, this.foodTrailHandler);
-          this.mapHandler.prepareForStart(IsChallenge);
-          if (IsChallenge) {
-            this.challengeHandler.handleStart(this.mapHandler.homeLocations);
-          }
-        } else {
-          this.mapHandler.findNewDecayableBlocks();
-          this.mapHandler.calculateFoodToStopTime();
+      if (!this.antHandler.antsSpawned) {
+        this.antHandler.spawnAnts(this.homeTrailHandler, this.foodTrailHandler);
+        this.mapHandler.prepareForStart(IsChallenge);
+        if (IsChallenge) {
+          this.challengeHandler.handleStart(this.mapHandler.homeLocations);
         }
+      } else {
+        this.mapHandler.findNewDecayableBlocks();
+        this.mapHandler.calculateFoodToStopTime();
+      }
 
       const ticksPerSecond = FrameRate * 1.5;
       const updateRate = 1000 / ticksPerSecond;
