2c315d145bb74016763365f149c47b1027046564
Cuzzo <2738751+Cuzzo01@users.noreply.github.com>
2022-01-05 20:28:26 -0600

    Start spaces service

---
+++ AntGameAPI/services/SpacesService.js
@@ -0,0 +1,19 @@
+const AWS = require("aws-sdk")
+
+class spacesService {
+    initializeConnection() {
+        if (!this.s3) {
+            this.spacesEndpoint = new AWS.Endpoint(process.env.DO_SPACES_ENDPOINT);
+            this.s3 = new AWS.S3({ endpoint: this.spacesEndpoint, accessKeyId: process.env.DO_SPACES_KEY, secretAccessKey: process.env.DO_SPACES_SECRET });
+        }
+    }
+
+    uploadFile(fileName, fileToUpload) {
+        this.s3.putObject({Bucket: process.env.DO_SPACES_NAME, Key: fileName, Body: JSON.stringify(fileToUpload), ACL: "public-read" }, (err, data) => {
+            if (err) return console.log(err)
+        })
+    }
+}
+
+const SpacesService = new spacesService();
+module.exports = SpacesService
