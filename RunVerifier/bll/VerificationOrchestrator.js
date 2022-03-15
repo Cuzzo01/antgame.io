7176fb1cdfdfd9f64ec0450aa337a031430a13da
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-03-14 20:38:48 -0500

    Revert "Add exception handling for run verification"
    This reverts commit c535635a045a73a5a406864f4b9b005a623f7254.

--- RunVerifier/bll/VerificationOrchestrator.js
+++ RunVerifier/bll/VerificationOrchestrator.js
@@ -4,47 +4,43 @@ const { SpawnVerificationRun } = require("./VerificationProcess");
 
 class VerificationOrchestrator {
   static async getAndProcessRunsToVerify() {
-    try {
-      Logger.logVerificationMessage({ message: "Starting to process runs" });
-      const runsToVerify = await getRunIDsToVerify();
+    Logger.logVerificationMessage({ message: "Starting to process runs" });
+    const runsToVerify = await getRunIDsToVerify();
 
-      if (runsToVerify.length > 0) {
-        Logger.logVerificationMessage({ message: `Got ${runsToVerify.length} runs to verify` });
+    if (runsToVerify.length > 0) {
+      Logger.logVerificationMessage({ message: `Got ${runsToVerify.length} runs to verify` });
 
-        const promises = new Map();
-        for (const run of runsToVerify) {
-          promises.set(run._id, SpawnVerificationRun({ runData: run }));
-        }
-
-        for (const [id, resultPromise] of promises) {
-          let result = false;
-          try {
-            result = await resultPromise;
-
-            if (result) {
-              await addTagToRun({ id, tag: { type: "run verified" } });
-            } else {
-              await addTagToRun({
-                id,
-                tag: {
-                  type: "failed verification",
-                  metadata: { reason: "simulated score did not match" },
-                },
-              });
-            }
-          } catch (e) {
-            Logger.logError("VerifyRun", e);
-          }
-          await unsetToVerifyFlag({ runID: id });
-        }
-      } else {
-        Logger.logVerificationMessage({ message: "No runs to verify" });
+      const promises = new Map();
+      for (const run of runsToVerify) {
+        promises.set(run._id, SpawnVerificationRun({ runData: run }));
       }
 
-      Logger.logVerificationMessage({ message: "Done" });
-    } catch (e) {
-      Logger.logError("VerificationOrchestrator", e);
+      for (const [id, resultPromise] of promises) {
+        let result = false;
+        try {
+          result = await resultPromise;
+
+          if (result) {
+            await addTagToRun({ id, tag: { type: "run verified" } });
+          } else {
+            await addTagToRun({
+              id,
+              tag: {
+                type: "failed verification",
+                metadata: { reason: "simulated score did not match" },
+              },
+            });
+          }
+        } catch (e) {
+          Logger.logError("VerifyRun", e);
+        }
+        await unsetToVerifyFlag({ runID: id });
+      }
+    } else {
+      Logger.logVerificationMessage({ message: "No runs to verify" });
     }
+
+    Logger.logVerificationMessage({ message: "Done" });
   }
 }
 module.exports = { VerificationOrchestrator };
