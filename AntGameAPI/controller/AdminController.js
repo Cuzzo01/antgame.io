0d6c067d80d513479ac1b7afaf576b82c09e285b
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-03-14 20:38:54 -0500

    Revert "Run verification in parallel"
    This reverts commit 4702bc9e4e495c0a5b2f11b688e5c6244cf462c4.

--- AntGameAPI/controller/AdminController.js
+++ AntGameAPI/controller/AdminController.js
@@ -17,7 +17,7 @@ const {
   updateFlagByID,
   getChampionshipListFromDB,
 } = require("../dao/AdminDao");
-const { getActiveChallenges, markRunForVerification } = require("../dao/ChallengeDao");
+const { getActiveChallenges } = require("../dao/ChallengeDao");
 const ObjectIDToNameHandler = require("../handler/ObjectIDToNameHandler");
 const LeaderboardHandler = require("../handler/LeaderboardHandler");
 const UserHandler = require("../handler/UserHandler");
@@ -394,25 +394,6 @@ async function getRunDetails(req, res) {
     res.sendStatus(500);
   }
 }
-
-async function patchRun(req, res) {
-  try {
-    const id = req.params.id;
-    const patchReq = req.body;
-
-    if (patchReq.toVerify) {
-      await markRunForVerification({ runID: id });
-    } else {
-      send400(res, "Unknown patch operation");
-      return;
-    }
-
-    res.sendStatus(200);
-  } catch (e) {
-    Logger.logError("AdminController.patchRun", e);
-    res.sendStatus(500);
-  }
-}
 //#endregion runs
 
 //#region championships
@@ -564,5 +545,4 @@ module.exports = {
   dumpLeaderboardCache,
   dumpUserCache,
   generateAndBindSolutionImage,
-  patchRun,
 };
