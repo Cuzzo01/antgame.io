f3a3847fd9ac77e35d2581e3d44c44ba979ef31e
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-03-26 15:29:47 -0500

    Add dirt to map generator

--- AntGameAPI/MapGenerator/MapGenerator.js
+++ AntGameAPI/MapGenerator/MapGenerator.js
@@ -29,6 +29,10 @@ const WallModifierBounds = [1.5, 2.5];
 const FoodFreqBounds = [5, 10];
 const FoodNoiseFloorBounds = [0.63, 0.73];
 const MinFoodGroupSizeBounds = [20, 45];
+const DirtChance = 0.75;
+const DirtNoiseFloorBounds = [0.55, 0.65];
+const DirtFreqBounds = [3.5, 4.5];
+const MinDirtGroupSize = 20;
 
 // currently (200, 112)
 const generateMap = (width, height) => {
@@ -69,6 +73,9 @@ const generateMap = (width, height) => {
     }
   }
 
+  RemoveGroupsSmallerThan("w", 10, mapData, " ");
+  RemoveGroupsSmallerThan(" ", 20, mapData, "w");
+
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       if (mapData[x][y] === " ") {
@@ -86,6 +93,30 @@ const generateMap = (width, height) => {
   }
 
   RemoveGroupsSmallerThan("f", MinFoodGroupSize, mapData, " ");
+
+  if (Math.random() < DirtChance) {
+    const DirtFreq = getRandomInRange(DirtFreqBounds[0], DirtFreqBounds[1]);
+    const DirtNoiseFloor = getRandomInRange(DirtNoiseFloorBounds[0], DirtNoiseFloorBounds[1]);
+    const DirtNoise = new Noise(DirtFreq);
+
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        if (mapData[x][y] === " ") {
+          const nx = x / width - 0.5;
+          const ny = y / height - 0.5;
+
+          const value = DirtNoise.getNoise(nx, ny);
+
+          if (value > DirtNoiseFloor) {
+            mapData[x][y] = "d";
+          }
+        }
+      }
+    }
+
+    RemoveGroupsSmallerThan("d", MinDirtGroupSize, mapData, " ");
+    RemoveGroupsSmallerThan(" ", 20, mapData, "d");
+  }
 
   if (CountOnMap("f", mapData) < MinFoodCount) return generateMap(width, height);
   else return mapData;
