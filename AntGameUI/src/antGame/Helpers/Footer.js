fd211ecf69b0697dc6b995b15e7a36bbab4e661c
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-08-04 18:13:43 -0500

    Add Discord link

--- AntGameUI/src/antGame/Helpers/Footer.js
+++ AntGameUI/src/antGame/Helpers/Footer.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getFlag } from "./FlagService";
 import styles from "./GenericStyles.module.css";
 import GitHubLogo from "./githubLogo.png";
+import DiscordLogo from "./discordLogo.png";
 
 const Footer = () => {
   const [shouldShow, setShouldShow] = useState(false);
@@ -22,7 +23,10 @@ const Footer = () => {
             <div>
               Questions/Feedback/Bugs: <a href="mailto:hi@antgame.io">hi@antgame.io</a>
             </div>
-            <div className={styles.githubLink}>
+            <div className={styles.links}>
+              <a href="https://discord.gg/mNpZdbtPt3" target="_blank" rel="noreferrer">
+                <img src={DiscordLogo} alt="Discord logo" />
+              </a>
               <a href="https://github.com/Cuzzo01/antgame.io" target="_blank" rel="noreferrer">
                 <img src={GitHubLogo} alt="GitHub logo" />
               </a>
