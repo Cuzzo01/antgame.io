de3f20bf6cfcfd47bbba1bfc2f2713aa0ff3e86f
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-08-04 21:39:21 -0500

    Fix null reference on PR endpoint

--- AntGameAPI/controller/ChallengeController.js
+++ AntGameAPI/controller/ChallengeController.js
@@ -511,13 +511,13 @@ async function getPRHomeLocations(req, res) {
     const challengeID = req.params.id;
 
     const prRun = await LeaderboardHandler.getLeaderboardEntryByUserID(challengeID, user.id);
-    const result = await ChallengeDao.getRunDataByRunId(prRun.runID);
-
-    if (!result) {
+    if (!prRun) {
       res.status(404);
       res.send("No PR found");
       return;
     }
+
+    const result = await ChallengeDao.getRunDataByRunId(prRun.runID);
     res.send({ locations: result.homeLocations, amounts: result.homeAmounts });
   } catch (e) {
     Logger.logError("ChallengeController.GetPRHomeLocations", e);
