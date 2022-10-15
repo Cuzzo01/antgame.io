440a3853ca47e8b7d4138c0b0aca337e3be530eb
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-10-15 09:24:55 -0500

    Update closeMessage logic

--- AntGameUI/src/antGame/AntGameHelpers/Challenge/ChallengeModal.jsx
+++ AntGameUI/src/antGame/AntGameHelpers/Challenge/ChallengeModal.jsx
@@ -4,6 +4,7 @@ import ChallengeHandler from "../../Challenge/ChallengeHandler";
 import AuthHandler from "../../Auth/AuthHandler";
 import GenericModal from "../../Helpers/GenericModal";
 import { getFlag } from "../../Helpers/FlagService";
+import { useCallback } from "react";
 
 const ChallengeModal = props => {
   const [isWrRun, setIsWrRun] = useState(false);
@@ -11,6 +12,35 @@ const ChallengeModal = props => {
   const [showRateLimitMessage, setShowRateLimitMessage] = useState(false);
   const [showRejectedMessage, setShowRejectedMessage] = useState(false);
   const [showAd, setShowAd] = useState(false);
+  const [closeMessage, setCloseMessage] = useState("Close");
+
+  const updateCloseMessage = useCallback(() => {
+    const score = props.challengeHandler?.score;
+    if (score) {
+      const scoreIsNice = score.toString().endsWith("69");
+      const scoreIsDoubleNice = score.toString().match(/69/gm)?.length === 2;
+
+      let message = "Close";
+      if (scoreIsDoubleNice) message = "Nice, Nice";
+      else if (scoreIsNice) message = "Nice";
+
+      setCloseMessage(message);
+    }
+  }, [props.challengeHandler?.score]);
+
+  const handleRunResponse = useCallback(
+    response => {
+      if (response === false) {
+        setIsWrRun(false);
+        setShowRateLimitMessage(false);
+        setShowRejectedMessage(false);
+      } else if (response.isWrRun) setIsWrRun(true);
+      else if (response === "rateLimit") setShowRateLimitMessage(true);
+      else if (response === "rejected") setShowRejectedMessage(true);
+      updateCloseMessage();
+    },
+    [updateCloseMessage]
+  );
 
   useEffect(() => {
     if (props.show) {
@@ -35,25 +65,14 @@ const ChallengeModal = props => {
       ChallengeHandler.removeRunResponseListener(runResponseId);
       ChallengeHandler.removeRecordListener(recordID);
     };
-  }, [props.show, props.challengeHandler?.isPB]);
+  }, [props.show, props.challengeHandler?.isPB, handleRunResponse]);
 
-  const handleRunResponse = response => {
-    if (response === false) {
-      setIsWrRun(false);
-      setShowRateLimitMessage(false);
-      setShowRejectedMessage(false);
-    } else if (response.isWrRun) setIsWrRun(true);
-    else if (response === "rateLimit") setShowRateLimitMessage(true);
-    else if (response === "rejected") setShowRejectedMessage(true);
-  };
-
-  const scoreIsNice = props.challengeHandler?.score.toString().endsWith("69");
   return (
     <div>
       {props.show ? (
         <GenericModal
           alwaysShow
-          closeMessage={scoreIsNice ? "Nice" : "Close"}
+          closeMessage={closeMessage}
           title={`Results: ${props.challengeHandler?.config.name}`}
           onHide={() => props.closeModal()}
           body={
