2f1895000cfd84ed4e30daaf457ea52a77977c4b
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-12-20 21:25:59 -0600

    Fix

--- AntGameUI/src/antGame/Helpers/Footer.js
+++ AntGameUI/src/antGame/Helpers/Footer.js
@@ -6,24 +6,22 @@ import RedditLogo from "./graphics/redditLogo.png";
 const Footer = () => {
   return (
     <div>
-      <div>
-        <hr />
-        <div className={styles.footerContainer}>
-          <span />
-          <div>
-            Questions/Feedback/Bugs: <a href="mailto:hi@antgame.io">hi@antgame.io</a>
-          </div>
-          <div className={styles.links}>
-            <a href="https://discord.gg/mNpZdbtPt3" target="_blank" rel="noreferrer">
-              <img src={DiscordLogo} alt="Discord logo" />
-            </a>
-            <a href="https://github.com/Cuzzo01/antgame.io" target="_blank" rel="noreferrer">
-              <img src={GitHubLogo} alt="GitHub logo" />
-            </a>
-            <a href="https://www.reddit.com/r/AntGame/" target="_blank" rel="noreferrer">
-              <img src={RedditLogo} alt="Reddit logo" />
-            </a>
-          </div>
+      <hr />
+      <div className={styles.footerContainer}>
+        <span />
+        <div>
+          Questions/Feedback/Bugs: <a href="mailto:hi@antgame.io">hi@antgame.io</a>
+        </div>
+        <div className={styles.links}>
+          <a href="https://discord.gg/mNpZdbtPt3" target="_blank" rel="noreferrer">
+            <img src={DiscordLogo} alt="Discord logo" />
+          </a>
+          <a href="https://github.com/Cuzzo01/antgame.io" target="_blank" rel="noreferrer">
+            <img src={GitHubLogo} alt="GitHub logo" />
+          </a>
+          <a href="https://www.reddit.com/r/AntGame/" target="_blank" rel="noreferrer">
+            <img src={RedditLogo} alt="Reddit logo" />
+          </a>
         </div>
       </div>
     </div>
