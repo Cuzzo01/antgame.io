ce0bbc8fa41dc53a0079148aff7d43a1d269b4e5
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2023-12-26 13:59:01 -0600

    fixed an off by 1

--- AntGameUI/src/antGame/AntGameHelpers/RunHistory/RunHistoryService.js
+++ AntGameUI/src/antGame/AntGameHelpers/RunHistory/RunHistoryService.js
@@ -2,59 +2,68 @@ import { getPreviousRunData } from "../../Challenge/ChallengeService";
 import EventBus from "../../Helpers/EventBus";
 
 export class RunHistoryService {
-    constructor(challengeId) {
-        this._challengeId = challengeId;
-        this._allRuns = [];
-        this._hasLoadedAllRuns = false;
-        this._apiPage = 1;
+  constructor(challengeId) {
+    this._challengeId = challengeId;
+    this._allRuns = [];
+    this._hasLoadedAllRuns = false;
+    this._apiPage = 1;
+  }
+
+  async loadMoreRuns() {
+    var result = await getPreviousRunData({
+      challengeId: this._challengeId,
+      pageIndex: this._apiPage,
+    });
+    this._apiPage++;
+    if (result) {
+      this._allRuns.push(...result.runs);
+      this._hasLoadedAllRuns = result.reachedEndOfBatch;
+    } else {
+      this._hasLoadedAllRuns = true;
+    }
+  }
+
+  async getRunsBetween(start, end) {
+    while (!this._hasLoadedAllRuns && this._allRuns.length < end) {
+      await this.loadMoreRuns();
     }
 
-    async loadMoreRuns() {
-        var result = await getPreviousRunData({
-            challengeId: this._challengeId,
-            pageIndex: this._apiPage,
-        });
-        this._apiPage++;
-        if (result) {
-            this._allRuns.push(...result.runs);
-            this._hasLoadedAllRuns = result.reachedEndOfBatch;
-        } else {
-            this._hasLoadedAllRuns = true;
-        }
-    }
+    var runsToSend = this._allRuns
+      .map((run, index) => {
+        return { ...run, index: index + 1 };
+      })
+      .slice(start, end);
+    return {
+      runs: runsToSend,
+      endReached: this._hasLoadedAllRuns,
+      numLoaded: this._allRuns.length,
+    };
+  }
 
-    async getRunsBetween(start, end) {
-        while (!this._hasLoadedAllRuns && this._allRuns.length < end - 1) {
-            await this.loadMoreRuns();
-        }
+  pushNewestRunToTop(artifact, response) {
+    var newRun = {
+      locations: artifact.HomeLocations,
+      amounts: JSON.parse(artifact.Snapshots?.finish[5]),
+      seed: artifact.GameConfig.seed,
+      compatibilityDate: artifact.GameConfig.compatibilityDate,
+      submissionTime: artifact.Timing.SystemStopTime,
+      score: artifact.Score,
+      pr: artifact.PB,
+      wr: response.isWrRun,
+    };
 
-        var runsToSend = this._allRuns.map((run, index) => { return { ...run, index: index + 1 } }).slice(start, end);
-        return { runs: runsToSend, endReached: this._hasLoadedAllRuns, numLoaded: this._allRuns.length };
-    }
+    this._allRuns = [newRun, ...this._allRuns];
 
-    pushNewestRunToTop(artifact, response) {
-        var newRun =
-        {
-            locations: artifact.HomeLocations,
-            amounts: JSON.parse(artifact.Snapshots?.finish[5]),
-            seed: artifact.GameConfig.seed,
-            compatibilityDate: artifact.GameConfig.compatibilityDate,
-            submissionTime: artifact.Timing.SystemStopTime,
-            score: artifact.Score,
-            pr: artifact.PB,
-            wr: response.isWrRun
-        };
+    EventBus.dispatch("runHistoryUpdated", {});
+  }
 
-        this._allRuns = [newRun, ...this._allRuns];
+  registerRunSubmittedListener() {
+    this._runAcceptedListenerId = EventBus.on("runAccepted", data =>
+      this.pushNewestRunToTop(data.artifact, data.response)
+    );
+  }
 
-        EventBus.dispatch("runHistoryUpdated", {});
-    }
-
-    registerRunSubmittedListener() {
-        this._runAcceptedListenerId = EventBus.on("runAccepted", (data) => this.pushNewestRunToTop(data.artifact, data.response));
-    }
-
-    removeRunSubmittedListener() {
-        EventBus.remove("runAccepted", this._runAcceptedListenerId);
-    }
+  removeRunSubmittedListener() {
+    EventBus.remove("runAccepted", this._runAcceptedListenerId);
+  }
 }
